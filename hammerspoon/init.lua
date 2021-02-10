58bd51227e4050030fd97602d43be69872ae3ddb
Mike Rapadas <mike@mrap.me>
2021-02-09 16:21:29 -0800

    Tapping ctrl without a key triggers escape

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,4 @@
 require 'reload-config'
 require 'hyper'
 require 'fn-arrows'
+hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
